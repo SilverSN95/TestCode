a1312482c922de224dee05151c61c9a7676f2158
Ben <bhbramhall@gmail.com>
2016-11-04 14:56:30 -0400

    Testing commit

--- RESTService.Lib/Routing.cs
+++ RESTService.Lib/Routing.cs
@@ -7,6 +7,7 @@ namespace WebService.Lib
 {
     public static class Routing
     {
+        //Constants
         public const string HelloWorldRoute = "/HelloWorld";
         public const string GetForecastIORoute = "/ForecastIO";
         public const string GetForecastWURoute = "/ForecastWU";
